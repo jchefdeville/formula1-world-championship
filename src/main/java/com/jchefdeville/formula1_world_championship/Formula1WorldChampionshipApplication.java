033ef2ffb3223be461a254751e96a3362eed34e0
unknown <jeremy.chefdeville@gmail.com>
2024-05-26 18:58:04 +0200

    Add springboot

---
+++ src/main/java/com/jchefdeville/formula1_world_championship/Formula1WorldChampionshipApplication.java
@@ -0,0 +1,13 @@
+package com.jchefdeville.formula1_world_championship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formula1WorldChampionshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Formula1WorldChampionshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jchefdeville/formula1_world_championship/Formula1WorldChampionshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jchefdeville.formula1_world_championship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formula1WorldChampionshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
