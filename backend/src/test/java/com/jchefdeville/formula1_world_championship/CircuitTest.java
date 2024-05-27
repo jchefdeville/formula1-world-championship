59a30faff65f0fcfbcdf4b9e4f4bdd77f287526d
unknown <jeremy.chefdeville@gmail.com>
2024-05-27 22:16:14 +0200

    Change csv files from data into src/main/resources folder. Add test to read CSV

--- backend/src/test/java/com/jchefdeville/formula1_world_championship/CircuitTest.java
+++ backend/src/test/java/com/jchefdeville/formula1_world_championship/CircuitTest.java
@@ -1,7 +1,14 @@
 package com.jchefdeville.formula1_world_championship;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 
@@ -12,5 +19,18 @@ class CircuitTest {
     @Test
     public void testHelloWorld() {
         logger.info("Hello World");
+        assertTrue(true);
+    }
+
+    @Test
+    public void tryOpeningCsv() {
+        try {
+            Path csvFile = Paths.get(getClass().getResource("/circuits.csv").toURI());
+            try (Stream<String> lines = Files.lines(csvFile)) {
+                lines.forEach(logger::info);
+            }
+        } catch (IOException | URISyntaxException e) {
+            assertTrue(false);
+        }
     }
 }
