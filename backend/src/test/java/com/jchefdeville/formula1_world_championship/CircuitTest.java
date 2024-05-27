36454eabff016bb36927de731df1710d5303b124
unknown <jeremy.chefdeville@gmail.com>
2024-05-27 22:00:30 +0200

    Add unit test

---
+++ backend/src/test/java/com/jchefdeville/formula1_world_championship/CircuitTest.java
@@ -0,0 +1,16 @@
+package com.jchefdeville.formula1_world_championship;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
+
+class CircuitTest {
+
+    private static final Logger logger = Logger.getLogger("CircuitTest");
+
+    @Test
+    public void testHelloWorld() {
+        logger.info("Hello World");
+    }
+}
